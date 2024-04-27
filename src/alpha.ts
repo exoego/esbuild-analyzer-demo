74d5b17c84b928eda38291dc537ce6d3d0966ac2
exoego <ytatsuno.jp@gmail.com>
2024-04-27 22:34:41 +0900

    wip

--- src/alpha.ts
+++ src/alpha.ts
@@ -1,13 +1,19 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const s3 = new S3Client();
+const dynamo = new DynamoDBClient();
 
 export const handler = async (event: any) => {
-	const result = await s3.send(
+	const putObjectResult = await s3.send(
 		new PutObjectCommand({
 			Bucket: "my-bucket",
 			Key: "my-key",
 		}),
 	);
-	return result;
+	const listTablesResult = await dynamo.send(new ListTablesCommand({}));
+	return {
+		putObjectResult,
+		listTablesResult,
+	};
 };
