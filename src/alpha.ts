43ac521af22d3d27cec76fd6c3414c1d409c6e61
exoego <ytatsuno.jp@gmail.com>
2024-04-28 09:20:10 +0900

    wip

--- src/alpha.ts
+++ src/alpha.ts
@@ -1,10 +1,14 @@
-import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { ListTablesCommand, DescribeTableCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const dynamo = new DynamoDBClient();
 
 export const handler = async (event: any) => {
 	const listTablesResult = await dynamo.send(new ListTablesCommand({}));
+	const createTableResult = await dynamo.send(new DescribeTableCommand({
+		TableName: "test-table"
+	}));
 	return {
 		listTablesResult,
+		createTableResult,
 	};
 };
