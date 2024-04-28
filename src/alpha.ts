fae54f0d93c3c294cb96c49cbca3e9dafc09d26b
exoego <ytatsuno.jp@gmail.com>
2024-04-28 15:44:38 +0900

    wip

--- src/alpha.ts
+++ src/alpha.ts
@@ -1,10 +1,14 @@
 import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 
 const dynamo = new DynamoDBClient();
+const s3 = new DynamoDBClient();
 
 export const handler = async (event: any) => {
 	const listTablesResult = await dynamo.send(new ListTablesCommand({}));
+	const listBucketsResult = await s3.send(new ListBucketsCommand({}));
 	return {
 		listTablesResult,
+		listBucketsResult,
 	};
 };
