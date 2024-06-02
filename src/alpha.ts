cb949cf0b84429754433bd9133c60d742d61904f
exoego <ytatsuno.jp@gmail.com>
2024-06-02 20:47:52 +0900

    bump

--- src/alpha.ts
+++ src/alpha.ts
@@ -1,10 +1,12 @@
-import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { ListTablesCommand, ListBackupsCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
 const dynamo = new DynamoDBClient();
 
 export const handler = async (event: any) => {
 	const listTablesResult = await dynamo.send(new ListTablesCommand({}));
+	const listBackupResult = await dynamo.send(new ListBackupsCommand({}));
 	return {
 		listTablesResult,
+		listBackupResult,
 	};
 };
