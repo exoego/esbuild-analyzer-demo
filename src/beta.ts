b776cfce9acfaa25b9f3b74df02a43d1e476bbcc
exoego <ytatsuno.jp@gmail.com>
2024-06-02 20:49:59 +0900

    bump

--- src/beta.ts
+++ src/beta.ts
@@ -1,7 +1,15 @@
+import { ListTablesCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { Hono } from "hono";
+
+const dynamo = new DynamoDBClient();
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hono!"));
+app.get("/", async (c) => {
+    const listTablesResult = await dynamo.send(new ListTablesCommand({}));
+    return c.json({
+        listTablesResult,
+    })
+});
 
 import { handle } from 'hono/aws-lambda'
 
